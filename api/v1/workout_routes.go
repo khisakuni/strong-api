813ecfbceb56a8910fb571b9d35b1ebfa86fa270
Kohei Hisakuni <kohei@philosophie.is>
2017-08-27 17:02:54 -0700

    Add delete endpoint

--- api/v1/workout_routes.go
+++ api/v1/workout_routes.go
@@ -13,6 +13,8 @@ func WorkoutRoutes(router *httprouter.Router) {
 	router.GET("/api/v1/workouts", workoutIndexAction)
 	router.GET("/api/v1/workouts/:id", workoutShowAction)
 	router.POST("/api/v1/workouts", workoutCreateAction)
+	router.PUT("/api/v1/workouts/:id", workoutUpdateAction)
+	router.DELETE("/api/v1/workouts/:id", workoutDestroyAction)
 }
 
 func workoutIndexAction(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
@@ -70,4 +72,44 @@ func workoutCreateAction(w http.ResponseWriter, r *http.Request, _ httprouter.Pa
 	w.Header().Set("content-type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(workout)
+}
+
+func workoutUpdateAction(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	id, err := strconv.Atoi(p.ByName("id"))
+	if err != nil {
+		InternalServerError(w)
+		return
+	}
+
+	var workout models.Workout
+	if err := json.NewDecoder(r.Body).Decode(&workout); err != nil {
+		InternalServerError(w)
+		return
+	}
+	workout.ID = id
+	err = database.Conn.Save(&workout).Error
+	if err != nil {
+		BadRequestError(w, err.Error())
+		return
+	}
+
+	w.Header().Set("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(workout)
+}
+
+func workoutDestroyAction(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	id, err := strconv.Atoi(p.ByName("id"))
+	if err != nil {
+		InternalServerError(w)
+		return
+	}
+	workout := models.Workout{ID: id}
+	err = database.Conn.Delete(&workout).Error
+	if err != nil {
+		BadRequestError(w, err.Error())
+		return
+	}
+	w.Header().Set("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
 }
